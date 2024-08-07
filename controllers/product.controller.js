96352d7a9b0c74924e2d411fd2083002ca889ec6
Rijo Sebastian <rijobdk@gmail.com>
2024-08-07 13:13:15 +0530

    feat: add delete and update product by id

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -49,7 +49,7 @@ exports.getProductsByUser = async (req, res) => {
   }
 };
 
-exports.getProductById = async(req, res) => {
+exports.getProductById = async (req, res) => {
   const { id } = req.params;
   const role = req.role;
   const userId = req.user;
@@ -58,8 +58,8 @@ exports.getProductById = async(req, res) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
-    const product = await Product.findOne({_id:id, owner:userId});
-    console.log(product, "product")
+    const product = await Product.findOne({ _id: id, owner: userId });
+    console.log(product, "product");
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
@@ -68,5 +68,65 @@ exports.getProductById = async(req, res) => {
     console.error("Error in getProductById:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
- 
+};
+
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const role = req.role;
+  const userId = req.user;
+  const { name, price, description, quantity } = req.body;
+
+  if (!name && !price && !description && !quantity) {
+    return res.status(400).json({ message: "Please add any field" });
+  }
+
+  if (role === "admin") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const product = await  Product.findOne({ _id: id, owner: userId });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    if (name) {
+      product.name = name;
+    }
+    if (price) {
+      product.price = price;
+    }
+    if (description) {
+      product.description = description;
+    }
+    if (quantity) {
+      product.quantity = quantity;
+    }
+    await product.save();
+    return res.status(200).json({ message: "Product updated successfully" });
+    
+  } catch (error) {
+    console.error("Error in updateProduct:", error);
+    return res.status(500).json({ message: "Internal server error" });
+    
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const role = req.role;
+  const userId = req.user;
+  if (role === "admin") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const product = await Product.findOne({ _id: id, owner: userId });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    await product.deleteOne({ _id: id, owner: userId });
+    return res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error in deleteProduct:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,10 +4,14 @@ const {
   createProduct,
   getProductsByUser,
   getProductById,
+  updateProduct,
+  deleteProduct,
 } = require("../controllers/product.controller");
 
 productRouter.post("/create", auth, createProduct);
 productRouter.get("/all", auth, getProductsByUser);
 productRouter.get("/:id", auth, getProductById);
+productRouter.put("/:id", auth, updateProduct);
+productRouter.delete("/:id", auth, deleteProduct);
 
 module.exports = productRouter;
