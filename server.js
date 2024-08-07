375a6f5a0942887d0b55c2d67b79eb8127ab302a
Rijo Sebastian <rijobdk@gmail.com>
2024-08-07 10:16:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const {connectDB} = require("./config/database")
+
+const app = express();
+
+dotenv.config()
+app.use(express.json())
+
+
+connectDB()
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
